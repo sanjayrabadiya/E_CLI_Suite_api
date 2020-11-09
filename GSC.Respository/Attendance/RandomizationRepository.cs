81e1463832997da0499d5f19f6b928ddce6041ba
Dell <Dell@DESKTOP-4F260VR>
2020-11-09 13:13:58 +0530

    randomization screeninng number bug solve

--- GSC.Respository/Attendance/RandomizationRepository.cs
+++ GSC.Respository/Attendance/RandomizationRepository.cs
@@ -138,7 +138,7 @@ namespace GSC.Respository.Attendance
             return result;
         }
 
-        public string Duplicate(Randomization objSave, int projectId)
+        public string Duplicate(RandomizationDto objSave, int projectId)
         {
             if (All.Any(x =>
                 x.Id != objSave.Id && x.ScreeningNumber == objSave.ScreeningNumber &&
