5891e5b3b0c24f1a8b028d41f5aaf2e7586337bf
Neel <Shree@DESKTOP-GGOILHM>
2021-01-18 16:22:38 +0530

    Schedule date display issue

--- GSC.Data/Dto/Project/Design/ProjectDesignTemplateMobileDto.cs
+++ GSC.Data/Dto/Project/Design/ProjectDesignTemplateMobileDto.cs
@@ -1,5 +1,6 @@
 ﻿using GSC.Data.Entities.Common;
 using GSC.Helper;
+using GSC.Shared.Extension;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -12,8 +13,20 @@ namespace GSC.Data.Dto.Project.Design
         public int ProjectDesignTemplateId { get; set; }
         public string TemplateName { get; set; }
         public ScreeningTemplateStatus Status { get; set; }
-        public DateTime? SubmittedDate { get; set; }
-        public DateTime? ScheduleDate { get; set; }
+        //public DateTime? SubmittedDate { get; set; }
+        private DateTime? _SubmittedDate { get; set; }
+        public DateTime? SubmittedDate
+        {
+            get => _SubmittedDate?.UtcDateTime();
+            set => _SubmittedDate = value?.UtcDateTime();
+        }
+        //public DateTime? ScheduleDate { get; set; }
         public int DesignOrder { get; set; }
+        private DateTime? _scheduleDate { get; set; }
+        public DateTime? ScheduleDate
+        {
+            get => _scheduleDate?.UtcDateTime();
+            set => _scheduleDate = value?.UtcDateTime();
+        }
     }
 }
