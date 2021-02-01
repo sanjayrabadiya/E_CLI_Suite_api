3c2e77e2de15f9e6c6d06ca1f57aef87a8f5e117
mansukh <mansukh_chhoala@yahoo.com>
2021-02-02 01:12:00 +0530

    Removed UTC

--- GSC.Shared/Extension/DateTimeOffsetExtensions.cs
+++ GSC.Shared/Extension/DateTimeOffsetExtensions.cs
@@ -18,18 +18,6 @@ namespace GSC.Shared.Extension
             return age;
         }
 
-        public static DateTime UtcDate(this DateTime dateTime)
-        {
-            if (dateTime.Kind == DateTimeKind.Unspecified)
-                dateTime = DateTime.SpecifyKind(dateTime, DateTimeKind.Utc);
-            return dateTime.ToUniversalTime();
-        }
-
-        public static DateTime? UtcDate(this DateTime? dateTime)
-        {
-            if (dateTime?.Kind == DateTimeKind.Unspecified)
-                dateTime = DateTime.SpecifyKind(Convert.ToDateTime(dateTime), DateTimeKind.Utc);
-            return dateTime?.ToUniversalTime();
-        }
+       
     }
 }
