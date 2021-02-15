768405cffd2d7c104dcc164d3ffcd1199b2df75d
Shree <Shree@GSCL1>
2021-02-15 12:14:55 +0530

    user patient differenciate

--- GSC.Respository/UserMgt/UserRepository.cs
+++ GSC.Respository/UserMgt/UserRepository.cs
@@ -108,13 +108,13 @@ namespace GSC.Respository.UserMgt
             //    Role = string.Join(", ",
             //        t.UserRoles.Where(x => x.DeletedDate == null).Select(s => s.SecurityRole.RoleName).ToList())
             //}).OrderByDescending(x => x.Id).ToList();
-            return All.Where(x => isDeleted ? x.DeletedDate != null : x.DeletedDate == null).
+            return All.Where(x => (isDeleted ? x.DeletedDate != null : x.DeletedDate == null) && x.UserType != UserMasterUserType.Patient).
                    ProjectTo<UserGridDto>(_mapper.ConfigurationProvider).OrderByDescending(x => x.Id).ToList();
         }
 
         public List<UserGridDto> GetPatients(PatientDto userDto)
         {
-            var UserID = _context.Randomization.Where(x => x.ProjectId == userDto.ProjectId).Select(x=>x.UserId).ToList();
+            var UserID = _context.Randomization.Where(x => x.ProjectId == userDto.ProjectId).Select(x => x.UserId).ToList();
             return All.Where(x => (userDto.IsDeleted ? x.DeletedDate != null : x.DeletedDate == null) && (UserID.Contains(x.Id))).
                    ProjectTo<UserGridDto>(_mapper.ConfigurationProvider).OrderByDescending(x => x.Id).ToList();
         }
