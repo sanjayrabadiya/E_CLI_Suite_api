67bef8b4f283d535a850e9a205662b7384e21faa
mansukh <mansukh_chhoala@yahoo.com>
2021-03-23 10:47:24 +0530

    scheudle changes

--- GSC.Respository/Screening/ScreeningVisitRepository.cs
+++ GSC.Respository/Screening/ScreeningVisitRepository.cs
@@ -316,8 +316,7 @@ namespace GSC.Respository.Screening
             }
             else
             {
-                scheduleVisit = All.Where(x => x.ScreeningEntryId == screeningEntryId && x.IsSchedule && x.Status == ScreeningVisitStatus.NotStarted).
-                 OrderBy(t => t.ScheduleDate).FirstOrDefault();
+                scheduleVisit = All.Where(x => x.ScreeningEntryId == screeningEntryId && x.IsSchedule && x.Status == ScreeningVisitStatus.NotStarted).FirstOrDefault();
                 if (scheduleVisit != null)
                 {
                     scheduleVisit.Status = ScreeningVisitStatus.Scheduled;
