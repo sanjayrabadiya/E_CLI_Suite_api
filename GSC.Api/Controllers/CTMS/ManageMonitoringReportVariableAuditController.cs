09e3c5ce64a1fd31d83675a7f415847860b4d923
Dell <Dell@DESKTOP-4F260VR>
2021-10-21 15:37:54 +0530

    ctms report audit

---
+++ GSC.Api/Controllers/CTMS/ManageMonitoringReportVariableAuditController.cs
@@ -0,0 +1,30 @@
+﻿using GSC.Api.Controllers.Common;
+using GSC.Data.Dto.Report;
+using GSC.Respository.CTMS;
+using GSC.Respository.Screening;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GSC.Api.Controllers.Screening
+{
+    [Route("api/[controller]")]
+    public class ManageMonitoringReportVariableAuditController : BaseController
+    {
+        private readonly IManageMonitoringReportVariableAuditRepository _manageMonitoringReportVariableAuditRepository;
+
+        public ManageMonitoringReportVariableAuditController(
+            IManageMonitoringReportVariableAuditRepository manageMonitoringReportVariableAuditRepository)
+        {
+            _manageMonitoringReportVariableAuditRepository = manageMonitoringReportVariableAuditRepository;
+        }
+
+        [HttpGet("{manageMonitoringReportVariableId}")]
+        public IActionResult Get(int manageMonitoringReportVariableId)
+        {
+            if (manageMonitoringReportVariableId <= 0) return BadRequest();
+
+            var auditsDto = _manageMonitoringReportVariableAuditRepository.GetAudits(manageMonitoringReportVariableId);
+
+            return Ok(auditsDto);
+        }
+    }
+}
--- GSC.Respository/CTMS/IManageMonitoringReportVariableAuditRepository.cs
+++ GSC.Respository/CTMS/IManageMonitoringReportVariableAuditRepository.cs
@@ -3,12 +3,13 @@ using GSC.Data.Entities.SupplyManagement;
 using System.Collections.Generic;
 using GSC.Common.GenericRespository;
 using GSC.Data.Entities.CTMS;
+using GSC.Data.Dto.CTMS;
 
 namespace GSC.Respository.CTMS
 {
     public interface IManageMonitoringReportVariableAuditRepository : IGenericRepository<ManageMonitoringReportVariableAudit>
     {
-        //IList<VerificationApprovalAuditDto> GetAudits(int VerificationApprovalTemplateValueId);
+        IList<ManageMonitoringReportVariableAuditDto> GetAudits(int ManageMonitoringReportVariableId);
         void Save(ManageMonitoringReportVariableAudit audit);
     }
 }
