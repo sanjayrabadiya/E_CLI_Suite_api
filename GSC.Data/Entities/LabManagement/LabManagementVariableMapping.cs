2283a12116716c18dfcc036a8b4103f03341679a
HGsc <hasmukh.prajapati@gsc-technologies.com>
2021-11-02 11:03:08 +0530

    lab management changes

--- GSC.Data/Entities/LabManagement/LabManagementVariableMapping.cs
+++ GSC.Data/Entities/LabManagement/LabManagementVariableMapping.cs
@@ -10,8 +10,6 @@ namespace GSC.Data.Entities.LabManagement
         public int LabManagementConfigurationId { get; set; }
         public int ProjectDesignVariableId { get; set; }
         public string TargetVariable { get; set; }
-       // public string Reason { get; set; }
-        public string Comment { get; set; }
         public LabManagementConfiguration LabManagementConfiguration { get; set; }
         public ProjectDesignVariable ProjectDesignVariable { get; set; }
         public int? AuditReasonId { get; set; }
