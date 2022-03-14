2345cc7cffc491451355a7fcd0c96a927655c7f9
gsc11 <gsc11@GSCLP11>
2022-03-14 12:17:42 +0530

    ctms manage monitiring change

--- GSC.Respository/CTMS/CtmsMonitoringReportRepository.cs
+++ GSC.Respository/CTMS/CtmsMonitoringReportRepository.cs
@@ -108,9 +108,9 @@ namespace GSC.Respository.CTMS
             string ActivityCode = tabNumber == 0 ? "act_001" : tabNumber == 1 ? "act_002" : tabNumber == 2 ? "act_003" :
                 tabNumber == 3 ? "act_004" : "act_005";
 
-            var CtmsActivity = _context.CtmsActivity.Where(x => x.ActivityCode == ActivityCode).FirstOrDefault();
+            var CtmsActivity = _context.CtmsActivity.Where(x => x.ActivityCode == ActivityCode && x.DeletedDate == null).FirstOrDefault();
 
-            var Activity = _context.Activity.Where(x => x.CtmsActivityId == CtmsActivity.Id && x.AppScreenId == appscreen.Id).FirstOrDefault();
+            var Activity = _context.Activity.Where(x => x.CtmsActivityId == CtmsActivity.Id && x.AppScreenId == appscreen.Id && x.DeletedDate == null).FirstOrDefault();
 
             var StudyLevelForm = _context.StudyLevelForm.Include(x => x.Activity)
                                 .Where(x => x.ProjectId == projectId && x.ActivityId == Activity.Id
