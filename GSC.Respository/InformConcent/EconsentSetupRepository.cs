dc1e5c1e208e06513714a96bfafba25dd4944a26
gsc11 <gsc11@GSCLP11>
2022-10-01 18:04:18 +0530

    econsent change

--- GSC.Respository/InformConcent/EconsentSetupRepository.cs
+++ GSC.Respository/InformConcent/EconsentSetupRepository.cs
@@ -145,11 +145,12 @@ namespace GSC.Respository.InformConcent
 
         public void SendDocumentEmailPatient(EconsentSetup econsent)
         {
-            //var patientstatus = _context.EconsentSetupPatientStatus.Where(x => x.EconsentDocumentId == econsent.Id).Select(x => x.PatientStatusId).ToList();
-            //var result = _context.Randomization.Where(x => x.Project.ParentProjectId == econsent.ProjectId && x.LanguageId == econsent.LanguageId && (patientstatus.Contains((int)x.PatientStatusId) || x.PatientStatusId== ScreeningPatientStatus.ConsentInProcess ||x.PatientStatusId== ScreeningPatientStatus.ReConsentInProcess)).Include(x => x.Project).ToList();
-            var result = _context.Randomization.Where(x => x.Project.ParentProjectId == econsent.ProjectId && x.DeletedDate==null && x.LanguageId == econsent.LanguageId && (x.PatientStatusId != ScreeningPatientStatus.Completed) && (x.PatientStatusId != ScreeningPatientStatus.Withdrawal) && (x.PatientStatusId!=ScreeningPatientStatus.PreScreening)).Include(x => x.Project).ToList();
+            var result = _context.Randomization.Where(x => x.Project.ParentProjectId == econsent.ProjectId && x.DeletedDate==null && x.LanguageId == econsent.LanguageId
+            && (x.PatientStatusId != ScreeningPatientStatus.Completed) && (x.PatientStatusId != ScreeningPatientStatus.Withdrawal)
+            //&& (x.PatientStatusId!=ScreeningPatientStatus.PreScreening)
+            ).Include(x => x.Project).ToList();
+
             string projectcode = _projectRepository.Find(econsent.ProjectId).ProjectCode;
-            //for (var i = 0; i <= result.Count - 1; i++)
             foreach (var item in result)
             {
                 //if (item.PatientStatusId == ScreeningPatientStatus.ConsentInProcess || item.PatientStatusId == ScreeningPatientStatus.ReConsentInProcess || item.PatientStatusId == ScreeningPatientStatus.ConsentCompleted)
@@ -169,7 +170,6 @@ namespace GSC.Respository.InformConcent
                 audit.Activity = ICFAction.Screened;
                 audit.PateientStatus = item.PatientStatusId;
                 _econsentReviewDetailsAuditRepository.Add(audit);
-                // }
                 if (item.Email != "")
                 {
                     string patientName = "";
