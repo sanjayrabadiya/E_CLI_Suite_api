fbc7018bdaa2c64fdc4bc1c9026b8781c5f86cda
Tinku.Mahato <Tinku.Mahato@GSC55>
2023-10-27 10:09:14 +0530

    Change video call configuration by company wise.

--- GSC.Respository/InformConcent/EConsentVideoRepository.cs
+++ GSC.Respository/InformConcent/EConsentVideoRepository.cs
@@ -8,6 +8,8 @@ using Microsoft.Extensions.Options;
 using AgoraIO.Rtm;
 using AgoraIO.Media;
 using System;
+using GSC.Respository.Configuration;
+using GSC.Data.Dto.Configuration;
 
 namespace GSC.Respository.InformConcent
 {
@@ -16,13 +18,16 @@ namespace GSC.Respository.InformConcent
         private readonly IOptions<VideoAPISettings> _VideoAPISettings;
         private readonly IJwtTokenAccesser _jwtTokenAccesser;
         private readonly IGSCContext _context;
+        private readonly IAppSettingRepository _appSettingRepository;
         public EConsentVideoRepository(IOptions<VideoAPISettings> VideoAPISettings,
                                     IGSCContext context,
+                                    IAppSettingRepository appSettingRepository,
                                     IJwtTokenAccesser jwtTokenAccesser) : base(context)
         {
             _VideoAPISettings = VideoAPISettings;
             _context = context;
             _jwtTokenAccesser = jwtTokenAccesser;
+            _appSettingRepository = appSettingRepository;
         }
         public EConsentVideoDto GenerateVideoSessionandToken(int ReceiverUserId)
         {
@@ -32,16 +37,17 @@ namespace GSC.Respository.InformConcent
             //string token = opentok.GenerateToken(sessionId);
             //string token2 = opentok.GenerateToken(sessionId);
             //uint privilegeExpiredTs =
+            var commonSettiongs = _appSettingRepository.Get<GeneralSettingsDto>(_jwtTokenAccesser.CompanyId);
             string channelName = $"CHANNEL-{_jwtTokenAccesser.UserId}";
             uint _expireTimeInSeconds = 3600;
             uint privilegeExpiredTs = _expireTimeInSeconds + (uint)Utils.getTimestamp();
-            string token1 = RtcTokenBuilder.buildTokenWithUID(_VideoAPISettings.Value.APP_ID, _VideoAPISettings.Value.APP_CERTIFICATE, channelName, (uint)_jwtTokenAccesser.UserId, RtcTokenBuilder.Role.RolePublisher, privilegeExpiredTs);
-            string token2 = RtcTokenBuilder.buildTokenWithUID(_VideoAPISettings.Value.APP_ID, _VideoAPISettings.Value.APP_CERTIFICATE, channelName, (uint)ReceiverUserId, RtcTokenBuilder.Role.RoleSubscriber, privilegeExpiredTs);
+            string token1 = RtcTokenBuilder.buildTokenWithUID(commonSettiongs.AgoraAppId, commonSettiongs.AgoraAppCertificate, channelName, (uint)_jwtTokenAccesser.UserId, RtcTokenBuilder.Role.RolePublisher, privilegeExpiredTs);
+            string token2 = RtcTokenBuilder.buildTokenWithUID(commonSettiongs.AgoraAppId, commonSettiongs.AgoraAppCertificate, channelName, (uint)ReceiverUserId, RtcTokenBuilder.Role.RoleSubscriber, privilegeExpiredTs);
             EConsentVideoDto eConsentVideoDto = new EConsentVideoDto();
             eConsentVideoDto.SessionId = channelName;
             eConsentVideoDto.Publishertoken = token1;
             eConsentVideoDto.Subscribertoken = token2;
-            eConsentVideoDto.ApiKey = "NA";
+            eConsentVideoDto.ApiKey = commonSettiongs.AgoraAppId;
             eConsentVideoDto.SenderUserId = _jwtTokenAccesser.UserId;
             eConsentVideoDto.ReceiverUserId = ReceiverUserId;
             eConsentVideoDto.RequestDelivered = false;
