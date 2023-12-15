2ad5cc3848cd77a55a4d8ebe3669c242435678dd
Mitul <project@gsc-technologies.com>
2023-12-15 16:17:47 +0530

    Name : Mitul Module : CTMS SubModule : GS1-I3201,GS1-I3200,GS1-I3199,GS1-I3198 Description : zoha bug And Budget Management

---
+++ GSC.Api/Controllers/CTMS/BudgetPlanerController.cs
@@ -0,0 +1,41 @@
+﻿
+using AutoMapper;
+using GSC.Api.Controllers.Common;
+using GSC.Common.UnitOfWork;
+using GSC.Domain.Context;
+using GSC.Respository.Configuration;
+using GSC.Respository.CTMS;
+using GSC.Shared.JWTAuth;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GSC.Api.Controllers.CTMS
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BudgetPlanerController : BaseController
+    {
+        private readonly IJwtTokenAccesser _jwtTokenAccesser;
+        private readonly IMapper _mapper;
+        private readonly IUnitOfWork _uow;
+        private readonly IGSCContext _context;
+        private readonly IStudyPlanTaskRepository _studyPlanTaskRepository;
+
+        public BudgetPlanerController(IUnitOfWork uow, IMapper mapper,
+            IJwtTokenAccesser jwtTokenAccesser, IStudyPlanRepository studyPlanRepository, IGSCContext context, IStudyPlanTaskRepository studyPlanTaskRepository,
+            IUploadSettingRepository uploadSettingRepository)
+        {
+            _uow = uow;
+            _mapper = mapper;
+            _jwtTokenAccesser = jwtTokenAccesser;
+            _context = context;
+            _studyPlanTaskRepository = studyPlanTaskRepository;
+        }
+
+        [HttpGet("{isDeleted:bool?}/{studyId:int}/{siteId:int}/{countryId:int}")]
+        public IActionResult Get(bool isDeleted, int studyId, int siteId, int countryId)
+        {
+            var studyplan = _studyPlanTaskRepository.getBudgetPlaner(isDeleted, studyId, siteId, countryId);
+            return Ok(studyplan);
+        }
+    }
+}
